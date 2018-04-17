62dd17cde0d4486f1c6521a9f7d838a585c11c1d
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-04-17 13:29:53 +0200

    Added: New Obstacle - Rotating Machine C (Hit Animation and Snd) Programmed: Machine behaviour

---
+++ Hunter/Assets/Scripts/Machine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Machine : MonoBehaviour
+{
+    AudioSource audioSource;
+    Animator animator;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != TagManager.arrow) return;
+
+        audioSource.Play();
+        animator.SetTrigger("ArrowHit");
+    }
+}
---
+++ Hunter/Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+    new Transform transform;
+    [SerializeField] float speed = 1;
+
+    private void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 1), speed);
+    }
+}
