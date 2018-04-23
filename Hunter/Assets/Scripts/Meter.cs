0508d96d5e71255ca5dc2c516b07a5ba6f56b36f
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-04-23 12:05:40 +0200

    Changed: Tense metter enabled only whet player is shooting

---
+++ Hunter/Assets/Scripts/Meter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+public class Meter : MonoBehaviour 
+{
+    [SerializeField] GameObject meterObject;
+
+    private void OnEnable()
+    {
+        BowEventManager.shooting.OnEnter += ShowMeter;
+        BowEventManager.shooting.OnExit += DisableMeter;
+        DisableMeter();
+    }
+
+    private void OnDisable()
+    {
+        BowEventManager.shooting.OnEnter -= ShowMeter;
+        BowEventManager.shooting.OnExit -= DisableMeter;
+    }
+
+    void ShowMeter()
+    {
+        meterObject.SetActive(true);
+    }
+
+    void DisableMeter()
+    {
+        meterObject.SetActive(false);
+    }
+}
